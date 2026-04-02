975aba575f914a7057e3d6e2c89e7115d03517d1
pierre-eitel <pierre.developer@hotmail.com>
2026-04-02 18:44:57 -0300

    Primeiro exercício

---
+++ DesafioPratico1/Exercicio1/BoasVindas.cs
@@ -0,0 +1,12 @@
+﻿namespace DesafioPratico1.Exercicio1;
+
+public class BoasVindas
+{
+    public void SejaBemvindo()
+    {
+        Console.Write("Digite seu nome: ");
+        string username = Console.ReadLine();
+
+        Console.WriteLine($"Olá, {username}! seja bem-vindo ao curso de C#!");
+    }
+}
--- DesafioPratico1/Program.cs
+++ DesafioPratico1/Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using DesafioPratico1.Exercicio1;
+using DesafioPratico1.Exercicio2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        BoasVindas boasVindas = new BoasVindas();
+        boasVindas.SejaBemvindo();
+
+        Concatenador concatenador = new Concatenador();
+        concatenador.NomeCompleto();
+    }
+}
