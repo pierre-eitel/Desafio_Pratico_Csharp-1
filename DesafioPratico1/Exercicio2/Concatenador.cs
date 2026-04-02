14faa5bbb3e80551a60d4a46ca7d0f30f199c724
pierre-eitel <pierre.developer@hotmail.com>
2026-04-02 18:45:36 -0300

    Segundo exercício

---
+++ DesafioPratico1/Exercicio2/Concatenador.cs
@@ -0,0 +1,16 @@
+﻿namespace DesafioPratico1.Exercicio2;
+
+public class Concatenador
+{
+    public void NomeCompleto()
+    {
+        Console.Write("Digite seu primeiro nome: ");
+        string nome = Console.ReadLine();
+
+        Console.Write("Digite seu sobrenome: ");
+        string sobrenome = Console.ReadLine();
+
+        string nomeCompleto = $"{nome} {sobrenome}";
+        Console.WriteLine($"Seu nome completo é: {nomeCompleto.Trim()}");
+    }
+}
